62261868de35694685117b0879e9fdce9d6ce86f
PavloBilenko <bilenko.pavlo@gmail.com>
2024-10-13 22:21:50 +0200

    -

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const path = require('path');
-const { fileURLToPath } = require('url');
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app = express();
 const port = 3000;
@@ -10,20 +11,23 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'src')));
 
+
 const comments = [
   {
     id: 1,
     userName: 'Natalia Shevchenko',
-    userPhoto: '/src/img/reviews-img/photo1.png',
-    text: 'Work with was extraordinary! He turned out to be a very competent and responsible specialist. The projects were completed on time and the result exceeded my expectations.',
+    userPhoto: '/src/img/reviews-img/photo1.png', 
+    text: 'Work with was extraordinary! He turned out to be a very competent and responsible specialist. The projects were completed on time and the result exceeded my expectations.'
   },
   {
     id: 2,
     userName: 'Dmytro Nazarenko',
-    userPhoto: '/src/img/reviews-img/photo2.png',
-    text: 'I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are undeniable. Cooperation with him always brings impressive results.',
-  },
+    userPhoto: '/src/img/reviews-img/photo2.png', 
+    text: 'I have the honor to recommend him as an exceptional professional in his field. His knowledge and expertise are undeniable. Cooperation with him always brings impressive results.'
+  }
 ];
+
+
 
 app.get('/comments', (req, res) => {
   res.json(comments);

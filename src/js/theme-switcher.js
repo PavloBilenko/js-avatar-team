41c35d4120e577f83385c63c01e4daf53e1e69e8
PavloBilenko <bilenko.pavlo@gmail.com>
2024-10-16 19:43:47 +0200

    -

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -3,8 +3,8 @@
   const body = document.body;
   const favicon = document.getElementById('favicon');
 
-  const faviconLight = 'public/favicon-light.svg';
-  const faviconDark = 'public/favicon-dark.svg';
+  const faviconLight = '/favicon-light.svg';
+  const faviconDark = '/favicon-dark.svg';
 
   const savedTheme = localStorage.getItem('theme');
 
